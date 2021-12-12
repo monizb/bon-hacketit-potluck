623933578e884c1a9932231ff373215413e3bad2
monizb <46259712+monizb@users.noreply.github.com>
2021-12-12 11:35:50 +0530

    Temp Changes

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -2,6 +2,7 @@ import React from 'react'
 import AppBar from "@material-ui/core/AppBar";
 import LoginButton from './Loginbtn';
 import bowl from "../Assets/pot3.png";
+import landimg from "../Assets/pablo-1041.png";
 import "../Styles/Landing.css"
 
 function Landing() {
@@ -13,11 +14,16 @@ function Landing() {
                     <p>PotLuck</p>
                 </div>
             </AppBar>
-            <div className="landing-content">
-                <h1>Meet Foodies Over Food </h1>
-                <LoginButton />
+            <div className="landing-content" style={{ display: "flex" }}>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                    <div style={{ width: "70%" }}>
+                        <h1>Meet Foodies Over Food </h1>
+                        <img src={landimg} className="landimg" />
+                    </div>
+                    <LoginButton />
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
 
